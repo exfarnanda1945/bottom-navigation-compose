97e33c38e71c3db3a6dd699d2678738da48eeaa3
miko <exfar@samir.co.id>
2023-08-02 13:27:24 +0700

    add: icon when selected

--- app/src/main/java/com/exfarnanda1945/bottomnavigationcompose/navigation/Route.kt
+++ app/src/main/java/com/exfarnanda1945/bottomnavigationcompose/navigation/Route.kt
@@ -4,14 +4,31 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Home
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class Route(
-    val routeId:String,
-    val title:String,
-    val icon:ImageVector
-){
-    object Home: Route("home","Home", Icons.Default.Home)
-    object Post: Route("post","Post", Icons.Default.AddCircle)
-    object Account: Route("account","Account", Icons.Default.AccountCircle)
+    val routeId: String,
+    val title: String,
+    val icon: ImageVector,
+    val iconSelected: ImageVector
+) {
+    object Home : Route(
+        "home",
+        "Home",
+        Icons.Outlined.Home,
+        Icons.Default.Home)
+    object Post : Route(
+        "post",
+        "Post",
+        Icons.Outlined.Add,
+        Icons.Default.AddCircle)
+    object Account :
+        Route(
+            "account",
+            "Account",
+            Icons.Outlined.AccountCircle,
+            Icons.Default.AccountCircle)
 }
