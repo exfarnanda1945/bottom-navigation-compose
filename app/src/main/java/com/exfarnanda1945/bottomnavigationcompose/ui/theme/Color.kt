491255538f945ca95a6d2ea157c960e1263d3b4f
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-08-02 08:58:47 +0700

    add: color resource

---
+++ app/src/main/java/com/exfarnanda1945/bottomnavigationcompose/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.exfarnanda1945.bottomnavigationcompose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Mint = Color(0xFF61ddbb)
+val GrapeFruit = Color(0xFFf76c83)
+val Aqua = Color(0xFF66d3f0)
