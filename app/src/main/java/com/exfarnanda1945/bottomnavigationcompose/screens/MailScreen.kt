dc4eeef9694d554fc8699660871d72ead22ecb4e
miko <exfar@samir.co.id>
2023-08-02 17:30:15 +0700

    add: mail screen

---
+++ app/src/main/java/com/exfarnanda1945/bottomnavigationcompose/screens/MailScreen.kt
@@ -0,0 +1,9 @@
+package com.exfarnanda1945.bottomnavigationcompose.screens
+
+import androidx.compose.runtime.Composable
+import com.exfarnanda1945.bottomnavigationcompose.ui.theme.Lavender
+
+@Composable
+fun MailScreen() {
+    Screen(title = "Mail Screen", color = Lavender)
+}
